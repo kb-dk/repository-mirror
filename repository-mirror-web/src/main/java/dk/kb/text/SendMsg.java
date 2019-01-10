b4931d508d4f1624bb2810e696f3ee9ab92916cf
Sigfrid Lundberg <siggelundberg@gmail.com>
2019-01-10 15:18:34 +0100

    code for submissions

---
+++ repository-mirror-web/src/main/java/dk/kb/text/SendMsg.java
@@ -0,0 +1,17 @@
+package dk.kb.text;
+
+import javax.jms.*;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.log4j.Logger;
+import org.w3c.dom.Node;
+
+public class SendMsg {
+
+    public SendMsg() {}
+
+    public void send(String msg) {
+
+    }
+
+}
