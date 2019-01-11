286e68ad61be41159f8c805d25b73c087ae64e0f
Sigfrid Lundberg <siggelundberg@gmail.com>
2019-01-11 14:43:39 +0100

    another source tree for the pull daemon and friends

--- ATTIC/initialize.sh
+++ ATTIC/initialize.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-mvn archetype:generate -DgroupId=dk.kb.repositoryMirror -DartifactId=repository-mirror-web -DarchetypeArtifactId=maven-archetype-webapp -DinteractiveMode=false
+# mvn archetype:generate -DgroupId=dk.kb.repositoryMirror -DartifactId=repository-mirror-web -DarchetypeArtifactId=maven-archetype-webapp -DinteractiveMode=false
+
+mvn archetype:generate -DgroupId=dk.kb.pullStuff -DartifactId=repository-pull -DarchetypeArtifactId=maven-archetype-plugin -DinteractiveMode=false
+
