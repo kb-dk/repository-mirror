be91abe86ab12d8362554d8cd6e280771007aa16
Sigfrid Lundberg <siggelundberg@gmail.com>
2019-01-16 15:47:26 +0100

    a better http client for web services

---
+++ repository-mirror-web/src/main/java/dk/kb/text/ApiClient.java
@@ -0,0 +1,28 @@
+package dk.kb.text;
+
+import org.apache.http.client.*;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.HttpEntity;
+import org.apache.http.util.EntityUtils;
+import org.apache.http.impl.client.*;
+
+public class ApiClient {
+
+    public ApiClient() {}
+
+    public String restGet(String URI) {
+	String contents = "";
+	try {
+	    HttpGet request = new HttpGet(URI);
+	    CloseableHttpClient httpClient = HttpClients.createDefault();
+	    CloseableHttpResponse response = httpClient.execute(request);
+	    HttpEntity entity = response.getEntity();
+	    contents = EntityUtils.toString(entity);
+	} catch(java.io.IOException e) {
+
+	}
+	return contents;
+    }
+
+}
