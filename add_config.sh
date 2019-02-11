e2973f84aef0da252cd3b2a62da74a30fb731a0e
Sigfrid Lundberg <siggelundberg@gmail.com>
2019-02-11 16:02:05 +0100

    handling config.xml

--- add_config.sh
+++ add_config.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+cp config_secret.xml database-push/src/main/resources/config.xml
 cp config_secret.xml repository-pull/src/main/resources/config.xml
 cp config_secret.xml repository-mirror-web/src/main/resources/config.xml
--- del_config.sh
+++ del_config.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+rm database-push/src/main/resources/config.xml
 rm repository-pull/src/main/resources/config.xml
 rm repository-mirror-web/src/main/resources/config.xml
