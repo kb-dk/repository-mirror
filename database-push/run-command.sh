1938837a922cdb4227ce1b5fda217b5f2c718066
Sigfrid Lundberg <siggelundberg@gmail.com>
2019-02-11 13:08:05 +0100

    building everything in one go

--- database-push/run-command.sh
+++ database-push/run-command.sh
@@ -4,5 +4,4 @@ TOMCAT=tomcat
 
 cd run_directory
 
-# /bin/su -c "java  -jar  ../target/repository-pull-1.0.one-jar.jar" $TOMCAT
-/bin/su -c "java  -cp  '../target/repository-pull-1.0.one-jar.jar:../target/repository-pull-1.0.jar'  dk.kb.pullStuff.RunLoad" $TOMCAT
+/bin/su -c "java  -jar  ../target/database-push-1.0.one-jar.jar" $TOMCAT
