cdd367a3457923901e8a7af804f78bb527d83cad
Sigfrid Lundberg <siggelundberg@gmail.com>
2019-02-22 10:32:37 +0100

    An outline of the API done

---
+++ database-push/src/main/java/dk/kb/dbStuff/SolrClient.java
@@ -0,0 +1,30 @@
+package dk.kb.dbStuff;
+
+public class SolrClient {
+
+    private String solr_server = "";
+    private String file_server = "";
+
+    public SolrClient() {}
+
+    public String getIndexEntry() {
+
+    }
+
+    public boolean sendIndexEntry() {
+
+    }
+	
+    public void setSolrServer(String server) {
+	this.solr_server = server;
+    }
+
+    public void setFileServer(String server) {
+	this.file_server = server;
+    }
+
+    
+
+
+
+}
