c701ce0dc6e503041abce0cf6b3dfbc40aacee72
Sigfrid Lundberg <siggelundberg@gmail.com>
2019-04-12 11:08:45 +0200

    no comments at this time

--- database-push/src/main/java/dk/kb/dbStuff/RunLoad.java
+++ database-push/src/main/java/dk/kb/dbStuff/RunLoad.java
@@ -75,7 +75,7 @@ public class RunLoad {
 
 		    // URIs for rest based communication with the backends
 
-		    String tmplt = consts.getConstants().getProperty("file.template");
+		    String tmplt  = consts.getConstants().getProperty("file.template");
 		    String db_uri = consts.getConstants().getProperty(target);
 
 		    String credField = target + ".credentials";
