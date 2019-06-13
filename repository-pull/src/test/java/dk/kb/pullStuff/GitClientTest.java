897f96deaea39cda600386db2562e4d6a2c7454b
Jonas Lindberg Frellesen <jolf@kb.dk>
2019-06-13 09:57:51 +0200

    Improved the GitClient test to be generic on to the current repository.

--- repository-pull/src/test/java/dk/kb/pullStuff/GitClientTest.java
+++ repository-pull/src/test/java/dk/kb/pullStuff/GitClientTest.java
@@ -4,19 +4,24 @@ import org.jaccept.structure.ExtendedTestCase;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.io.File;
 import java.util.UUID;
 
 public class GitClientTest extends ExtendedTestCase {
 
-    @Test
+    @Test//(enabled = false)
     public void testInstantiation() {
-        ConfigurableConstants.getInstance().getConstants().setProperty("data.home", "/home/jolf/git/");
-        GitClient client = new GitClient("Yggdrasil");
+        File dir = new File("..");
+
+        ConfigurableConstants.getInstance().getConstants().setProperty("data.home", dir.getAbsoluteFile().getParentFile().getParentFile().getParentFile().getAbsolutePath());
+        GitClient client = new GitClient("repository-mirror");
 
         Assert.assertNotNull(client.git);
-//        client.
 
-        System.err.println(client.gitBranches());
+        String branches= client.gitBranches();
+        Assert.assertNotNull(branches);
+        Assert.assertTrue(branches.contains("master"), "must contain a master branch");
+        // System.err.println(branches);
     }
 
     @Test(expectedExceptions = IllegalStateException.class)
