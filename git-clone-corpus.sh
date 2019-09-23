f11b206215b083a130a5e71fe2379de87f6c8569
Sigfrid Lundberg <siggelundberg@gmail.com>
2019-09-23 15:35:09 +0200

    making files a bit more transparent, we hope

--- git-clone-corpus.sh
+++ git-clone-corpus.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 git clone https://github.com/Det-Kongelige-Bibliotek/public-adl-text-sources.git
 git clone https://github.com/Det-Kongelige-Bibliotek/adl-text-sources.git
 git clone https://github.com/Det-Kongelige-Bibliotek/other_tei_projects.git
