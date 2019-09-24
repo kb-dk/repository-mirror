377e28c33cecffe8a0784541893427657b6bccaa
Sigfrid Lundberg <siggelundberg@gmail.com>
2019-09-24 12:29:23 +0200

    real silly bug

--- git-branch-corpus.sh
+++ git-branch-corpus.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cd public-adl-text-sources ; git branch installed_corpus
-cd adl-text-sources.git ; git branch installed_corpus
-cd other_tei_projects ; git branch installed_corpus
-cd SKS_tei ; git branch installed_corpus
-cd trykkefrihedsskrifter ; git branch installed_corpus
+(cd public-adl-text-sources ; git branch installed_corpus)
+(cd adl-text-sources ; git branch installed_corpus)
+(cd other_tei_projects ; git branch installed_corpus)
+(cd SKS_tei ; git branch installed_corpus)
+(cd trykkefrihedsskrifter ; git branch installed_corpus)
