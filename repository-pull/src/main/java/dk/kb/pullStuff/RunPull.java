58935448db1667717f657e455038a19cbc855654
Jonas Lindberg Frellesen <jolf@kb.dk>
2019-11-26 11:33:30 +0100

    Restructured some code and added synchronization. Also made a couple of tests.

--- repository-pull/src/main/java/dk/kb/pullStuff/RunPull.java
+++ repository-pull/src/main/java/dk/kb/pullStuff/RunPull.java
@@ -1,6 +1,7 @@
 package dk.kb.pullStuff;
 
 import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.camel.Produce;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 
@@ -22,17 +23,35 @@ import java.util.Properties;
  */
 public class RunPull {
 
+	/*
+      We have different branches, locally and
+      remotely. Two are the ones we work with. One is
+      in our remote git text repository and one in the
+      remote repository. Their names are in the String variables
+
+      publishedBranch
+
+      and
+
+      branch
+
+      respectively.
+
+      The former is in the configuration file the
+      latter comes in a message from ActiveMQ
+
+    */
+
 	private static ConfigurableConstants consts = ConfigurableConstants.getInstance();
 	private static Logger logger = configureLog4j();
 
 	public static void main(String args[]) {
 
-		String host = System.getProperty("queue.uri");
-		if (host == null) host = consts.getConstants().getProperty("queue.uri");
+		String host = getHost();
 		ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory(host);
 		Connection connection = null;
 		Session session = null;
-		MessageConsumer pull_consumer = null;
+		MessageConsumer pullConsumer = null;
 		MessageProducer producer = null;
 
 		try {
@@ -41,161 +60,23 @@ public class RunPull {
 
 			session                      = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
 			String queue                 = consts.getConstants().getProperty("queue.name");
-			Destination pull_destination = session.createQueue(queue);
+			Destination pullDestination = session.createQueue(queue);
 			String push_queue            = consts.getConstants().getProperty("queue.load.name");
-			Destination push_destination = session.createQueue(push_queue);
-			pull_consumer                = session.createConsumer(pull_destination);
+			Destination pushDestination = session.createQueue(push_queue);
+			pullConsumer                = session.createConsumer(pullDestination);
 
-			producer = session.createProducer(push_destination);
+			producer = session.createProducer(pushDestination);
 			producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
 
 			while (true) {
-				String msg = "";
 				try {
-
-		    /*
-		      We have different branches, locally and
-		      remotely. Two are the ones we work with. One is
-		      in our remote git text repository and one in the
-		      remote repository. Their names are in the String variables
-
-		      publishedBranch
-
-		      and 
-
-		      branch
-
-		      respectively.
-
-		      The former is in the configuration file the
-		      latter comes in a message from ActiveMQ
-		      
-		    */
-
 					logger.info("Waiting for next message");
-					Message message = pull_consumer.receive();
-					if (message instanceof TextMessage) {
-						TextMessage textMessage = (TextMessage) message;
-						msg = textMessage.getText();
-						logger.info("Received: " + msg);
-						String reg = ";";
-						String collection  = msg.split(reg)[0]; // this is ADL or SKS etc
-						String repository  = msg.split(reg)[1]; // the git URI
-						String branch      = msg.split(reg)[2]; // the branch containing the desired data
-						String target      = msg.split(reg)[3]; // where we are going to deposit those data
+					Message message = pullConsumer.receive();
 
-						// Initialize our git gateway
-						logger.info("Setting repository: " + repository);
-						GitClient git = new GitClient(repository);
-
-						// This branch is used for storing data prior
-						// to loading it into eXist and Solr
-
-						String publishedBranch = consts.getConstants().getProperty("published.branch");
-
-						// Tell our git gateway the names of the two branches
-						git.setBranch(branch);
-						git.setPublishedBranch(publishedBranch);
-
-						// OK, first we fetch. We'll basically get
-						// everything that has happened since last
-						// fetch.
-						logger.info(git.gitFetch());
-
-						// branch, checkout the desired branch and do
-						// a pull
-
-						logger.info(git.gitCheckOut());
-						logger.info(git.gitPull());
-
-						// publishedBranch. This is where we are doing
-						// the work. There isn't any corresponding
-						// branch remotely so there is no need to
-						// pull.
-						//
-						// This branch should reflect the status of
-						// the last import
-
-						logger.info(git.gitCheckOutPublished());
-
-						// Now we have the two branches. In spite of
-						// the name gitLog() this is calculating a
-						// diff, not a log. This is actually returning a
-						// map between an object and operation.
-
-						java.util.HashMap<String,String> op = git.gitLog();
-
-						// This resets the publishedBranch to the
-						// state of the remote master. I.e., we have
-						// exactly the same data as in branch master.
-						logger.info(git.gitResetTo("origin/master"));
-
-						// Now we merge the remote desired branch with
-						// publishedBranch
-						//
-						// by doing this in this odd order, we'll be
-						// able to take into account the fact that the
-						// database might contain earlier
-						// changes. These will be overwritten below
-						// when we queue up the operations in the op
-						// HashMap for execution in the next step in
-						// the "conveyor belt"
-
-						logger.info(git.gitMergeToPublished(branch));
-
-						if(op.isEmpty()) {
-							logger.info("OK nothing to do");
-						} else {
-							logger.info("found operations");
-							java.util.Iterator<String> keys = op.keySet().iterator();
-							while(keys.hasNext()) {
-								String key = keys.next();
-								String theMessage
-										= collection + ";"
-										+ repository + ";"
-										+ branch     + ";"
-										+ target     + ";"
-										+ key        + ";"
-										+ op.get(key);
-								logger.info("about to send text msg = " + theMessage);
-								try {
-									TextMessage text_message = session.createTextMessage(theMessage);
-									producer.send(text_message);
-									logger.debug("text message sent to jms queue");
-								} catch (JMSException jme) {
-									jme.printStackTrace();
-									logger.error("could not text send message to queue");
-								}
-							}
-
-							// We've got all files, then we just send a message that it is time
-							// to run a commit
-							String finalMessage
-									= collection + ";"
-									+ repository + ";"
-									+ branch     + ";"
-									+ target     + ";"
-									+ ""         + ";"
-									+ "COMMIT";
-							logger.info("about to send final text msg = " + finalMessage);
-							try {
-								TextMessage text_message = session.createTextMessage(finalMessage);
-								producer.send(text_message);
-								logger.debug("text message sent to jms queue");
-							} catch (JMSException jme) {
-								jme.printStackTrace();
-								logger.error("could not text send message to queue");
-							}
-						}
-					} else {
-						msg = message.toString();
-					}
-
+					handleMessage(producer, session, message);
 				} catch (Exception e) {
-					logger.error("Error connecting  "+e);
-					logger.error("Waiting 60 sek and try again");
+					logger.error("Error connecting. Waiting 60 sek and try again.", e);
 
-					e.printStackTrace();
 					Thread.sleep(60000);
 				}
 			}
@@ -203,12 +84,135 @@ public class RunPull {
 			logger.fatal("Stopping execution ",e);
 		} finally {
 			try {
-				pull_consumer.close();
+				pullConsumer.close();
 				session.close();
 				connection.close();
+				producer.close();
 			} catch (Exception e) {
 				logger.fatal("error while shutting down ",e);
 			}
+		}
+	}
+
+	protected static synchronized void handleMessage(MessageProducer producer, Session session, Message message)
+			throws JMSException {
+		String msg = "";
+		if (message instanceof TextMessage) {
+			TextMessage textMessage = (TextMessage) message;
+			msg = textMessage.getText();
+			logger.info("Received: " + msg);
+			String reg = ";";
+			String[] split = msg.split(reg);
+			if(split.length < 4) {
+				throw new IllegalArgumentException("Invalid message. Does not contain 4 parts. '" + msg + "'");
+			}
+			String collection  = msg.split(reg)[0]; // this is ADL or SKS etc
+			String repository  = msg.split(reg)[1]; // the git URI
+			String branch      = msg.split(reg)[2]; // the branch containing the desired data
+			String target      = msg.split(reg)[3]; // where we are going to deposit those data
+
+			performPull(producer, session, collection, repository, branch, target);
+		} else {
+			logger.error("Cannot handle non-text-message: " + message.toString());
+			throw new IllegalStateException("Cannot handle non-text-message: " + message.toString());
+		}
+	}
+
+	protected static void performPull(MessageProducer producer, Session session, String collection, String repository,
+									  String branch, String target) {
+		// Initialize our git gateway
+		logger.info("Setting repository: " + repository);
+		GitClient git = new GitClient(repository);
+
+		// This branch is used for storing data prior
+		// to loading it into eXist and Solr
+
+		String publishedBranch = consts.getConstants().getProperty("published.branch");
+
+		// Tell our git gateway the names of the two branches
+		git.setBranch(branch);
+		git.setPublishedBranch(publishedBranch);
+
+		// OK, first we fetch. We'll basically get
+		// everything that has happened since last
+		// fetch.
+		logger.info(git.gitFetch());
+
+		// branch, checkout the desired branch and do
+		// a pull
+
+		//git.gitPullAll();
+		logger.info(git.gitCheckOut());
+		logger.info(git.gitPull());
+
+		// publishedBranch. This is where we are doing
+		// the work. There isn't any corresponding
+		// branch remotely so there is no need to
+		// pull.
+		//
+		// This branch should reflect the status of
+		// the last import
+
+		logger.info(git.gitCheckOutPublished());
+
+		// Now we have the two branches. In spite of
+		// the name gitLog() this is calculating a
+		// diff, not a log. This is actually returning a
+		// map between an object and operation.
+
+		java.util.HashMap<String, String> op = git.gitLog();
+
+		// This resets the publishedBranch to the
+		// state of the remote master. I.e., we have
+		// exactly the same data as in branch master.
+		logger.info(git.gitResetTo("origin/master"));
+
+		// Now we merge the remote desired branch with
+		// publishedBranch
+		//
+		// by doing this in this odd order, we'll be
+		// able to take into account the fact that the
+		// database might contain earlier
+		// changes. These will be overwritten below
+		// when we queue up the operations in the op
+		// HashMap for execution in the next step in
+		// the "conveyor belt"
+
+		logger.info(git.gitMergeToPublished(branch));
+
+		if (op.isEmpty()) {
+			logger.info("OK nothing to do");
+		} else {
+			logger.info("found operations");
+			java.util.Iterator<String> keys = op.keySet().iterator();
+			while (keys.hasNext()) {
+				String key = keys.next();
+				sendMessage(producer, session, collection, repository, branch, target, key, op.get(key));
+			}
+
+			// We've got all files, then we just send a message that it is time
+			// to run a commit
+			logger.info("Sending final message.");
+			sendMessage(producer, session, collection, repository, branch, target, "", "COMMIT");
+		}
+	}
+
+	protected static void sendMessage(MessageProducer producer, Session session, String collection, String repository,
+							   String branch, String target, String key, String operation) {
+		String message = collection + ";"
+				+ repository + ";"
+				+ branch + ";"
+				+ target + ";"
+				+ key + ";"
+				+ operation;
+		logger.info("sending message = " + message);
+		try {
+			TextMessage text_message = session.createTextMessage(message);
+			producer.send(text_message);
+			logger.debug("text message sent to jms queue");
+		} catch (JMSException jme) {
+			jme.printStackTrace();
+			logger.error("could not text send message to queue");
 		}
 	}
 
@@ -233,6 +237,14 @@ public class RunPull {
 		Logger logger = Logger.getLogger(RunPull.class);
 		logger.info("logging at level " + level + " in file " + file + "\n");
 		return logger;
+	}
+
+	private static String getHost() {
+		String host = System.getProperty("queue.uri");
+		if (host == null) {
+			return consts.getConstants().getProperty("queue.uri");
+		}
+		return host;
 	}
 
 	private static boolean wasItASuccess(InputStream response, Logger logger) {
