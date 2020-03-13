c6387d92a859104fbb01e42dec8d15853b40b3df
Jonas Lindberg Frellesen <jolf@kb.dk>
2020-03-13 13:07:55 +0100

    Fixed error in run script

--- text-service-backend/run-command.sh
+++ text-service-backend/run-command.sh
@@ -4,9 +4,6 @@ TOMCAT=tomcat
 
 cd run_directory
 
-/bin/su -c "java  -jar  ../target/repository-pull-1.0.one-jar.jar" $TOMCAT
+/bin/su -c "java  -jar  ../target/text-service-backend-1.0.one-jar.jar" $TOMCAT
 
-# from database push
-
-/bin/su -c "java  -jar  ../target/database-push-1.0.one-jar.jar" $TOMCAT
 
